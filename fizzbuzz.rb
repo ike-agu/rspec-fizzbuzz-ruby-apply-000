ee013ca2be233d9f67fc885184913684d44dfc8a
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-28 22:59:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,12 @@
 
 def fizzbuzz(int)
   
-  if int % 3 == 0
-    "Fizz"
-  elsif  int % 5 == 0
+  if int  %3 == 0 || int % 5 == 0
+    "FizzBuzz"
+  elsif  int % 3 == 0
     "Buzz"
   
-  elsif int / 15 == 3 && int / 15 == 5
+  elsif int % 5 == 0
     
     "FizzBuzz"
 elsif int % 4 ==0
